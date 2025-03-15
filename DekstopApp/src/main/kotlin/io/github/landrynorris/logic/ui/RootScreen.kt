3161e3b46efa0d01aa5984dfc30aa2742a2b54ae
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-03-15 17:28:02 -0500

    Improve sensor card appearance

--- DekstopApp/src/main/kotlin/io/github/landrynorris/logic/ui/RootScreen.kt
+++ DekstopApp/src/main/kotlin/io/github/landrynorris/logic/ui/RootScreen.kt
@@ -1,15 +1,19 @@
 package io.github.landrynorris.logic.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import io.github.landrynorris.logic.logic.RootLogic
@@ -37,18 +41,27 @@ fun RootScreen(logic: RootLogic) {
 fun SelectDeviceScreen(state: RootState, selectDevice: (String) -> Unit) {
     Row {
         for(sensor in state.sensors) {
-            Sensor(sensor, onClick = { selectDevice(sensor.serialNumber) })
+            SensorCard(sensor, onClick = { selectDevice(sensor.serialNumber) })
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Sensor(sensorState: SensorState, onClick: () -> Unit) {
-    Card(onClick = onClick) {
-        Column(modifier = Modifier.padding(10.dp)) {
-            Text(sensorState.serialNumber)
-            Text(sensorState.path)
+fun SensorCard(sensorState: SensorState, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(6.dp)
+            .clickable(onClick = onClick)
+            .background(Color.White),
+        elevation = 10.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(text = sensorState.name, style = MaterialTheme.typography.body1)
+            Text(text = "S/N: ${sensorState.serialNumber}", style = MaterialTheme.typography.subtitle2)
+            Text(text = "Path: ${sensorState.path}", style = MaterialTheme.typography.caption)
         }
     }
 }
