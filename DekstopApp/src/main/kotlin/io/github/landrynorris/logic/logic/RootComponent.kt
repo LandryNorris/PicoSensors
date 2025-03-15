aac1f5bf0357d32d82884f8f26ece49822df0c70
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-03-15 11:24:14 -0500

    Use proper navigation from decompose

--- DekstopApp/src/main/kotlin/io/github/landrynorris/logic/logic/RootComponent.kt
+++ DekstopApp/src/main/kotlin/io/github/landrynorris/logic/logic/RootComponent.kt
@@ -1,7 +1,8 @@
 package io.github.landrynorris.logic.logic
 
 import com.arkivanov.decompose.ComponentContext
-import com.arkivanov.decompose.childContext
+import com.arkivanov.decompose.router.slot.*
+import com.arkivanov.decompose.value.Value
 import io.github.landrynorris.logic.SensorDetector
 import io.github.landrynorris.logic.sensors.PicoSensor
 import io.github.landrynorris.logic.sensors.UnknownPicoSensor
@@ -13,8 +14,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
 
 interface RootLogic {
+    val child: Value<ChildSlot<SensorConfig, DeviceScreenComponent>>
     val state: StateFlow<RootState>
     fun beginDetectingDevices()
     fun selectDevice(serialNumber: String)
@@ -24,21 +27,25 @@ class RootComponent(context: ComponentContext): ComponentContext by context, Roo
     override val state: MutableStateFlow<RootState> = MutableStateFlow(RootState())
     private val componentDetectionScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
+    private val navigation = SlotNavigation<SensorConfig>()
+
+    override val child = childSlot(
+        source = navigation,
+        serializer = SensorConfig.serializer(),
+        handleBackButton = true,
+        childFactory = { config, component ->
+            val sensor = getSensorBySerial(config.serialNumber)
+            sensor ?: error("No sensor found with serial number: ${config.serialNumber}")
+
+            createComponentForSensor(sensor, component)
+        }
+    )
+
     override fun beginDetectingDevices() {
         componentDetectionScope.launch {
             SensorDetector.sensorFlow.collect { sensors ->
                 state.update { state ->
-                    val remainingComponents = state.currentComponents.filter {
-                        sensors.any { sensor -> sensor.serialDevice.serialNumber == it.serialNumber }
-                    }
-                    val newComponents = sensors.filter { sensor ->
-                        state.currentComponents.none { it.serialNumber == sensor.serialDevice.serialNumber }
-                    }.map {
-                        createComponentForSensor(sensor = it)
-                    }
-
                     state.copy(
-                        currentComponents = remainingComponents + newComponents,
                         sensors = sensors.map { it.toSensorState() }
                     )
                 }
@@ -47,24 +54,47 @@ class RootComponent(context: ComponentContext): ComponentContext by context, Roo
     }
 
     override fun selectDevice(serialNumber: String) {
-        state.update {
-            val component = it.currentComponents.firstOrNull { component -> component.serialNumber == serialNumber }
-
-            it.copy(activeComponent = component)
+        val sensor = getSensorBySerial(serialNumber)
+        sensor?.let {
+            val config = createConfigFromSensor(it)
+            navigation.activate(config)
         }
     }
 
     private fun onScreenBack() {
-        state.update { it.copy(activeComponent = null) }
+        navigation.dismiss()
     }
 
-    private fun createComponentForSensor(sensor: PicoSensor): DeviceScreenComponent {
+    private fun createComponentForSensor(sensor: PicoSensor, componentContext: ComponentContext): DeviceScreenComponent {
         return when(sensor) {
-            is VoltageSensor -> VoltageComponent(sensor, ::onScreenBack, childContext("V ${sensor.serialDevice.serialNumber}"))
-            is UnknownPicoSensor -> UnknownPicoComponent(sensor, ::onScreenBack, childContext("U ${sensor.serialDevice.serialNumber}"))
+            is VoltageSensor -> VoltageComponent(sensor, ::onScreenBack, componentContext)
+            is UnknownPicoSensor -> UnknownPicoComponent(sensor, ::onScreenBack, componentContext)
             else -> error("No component defined for sensor $sensor")
         }
     }
+
+    private fun createConfigFromSensor(sensor: PicoSensor): SensorConfig {
+        return when(sensor) {
+            is VoltageSensor -> SensorConfig.VoltageSensorConfig(sensor.serialDevice.serialNumber)
+            is UnknownPicoSensor -> SensorConfig.UnknpwnPicoSensorConfig(sensor.serialDevice.serialNumber)
+            else -> error("No config type found for sensor: $sensor")
+        }
+    }
+
+    private fun getSensorBySerial(serialNumber: String): PicoSensor? {
+        return SensorDetector.sensorFlow.value.firstOrNull { it.serialDevice.serialNumber == serialNumber }
+    }
+}
+
+@Serializable
+sealed interface SensorConfig {
+    val serialNumber: String
+
+    @Serializable
+    data class VoltageSensorConfig(override val serialNumber: String): SensorConfig
+
+    @Serializable
+    data class UnknpwnPicoSensorConfig(override val serialNumber: String): SensorConfig
 }
 
 fun PicoSensor.toSensorState(): SensorState {
@@ -74,7 +104,6 @@ fun PicoSensor.toSensorState(): SensorState {
 data class SensorState(val serialNumber: String, val path: String)
 
 data class RootState(
-    val currentComponents: List<DeviceScreenComponent> = listOf(),
     val activeComponent: DeviceScreenComponent? = null,
     val sensors: List<SensorState> = listOf()
 )
--- DekstopApp/src/main/kotlin/io/github/landrynorris/logic/ui/RootScreen.kt
+++ DekstopApp/src/main/kotlin/io/github/landrynorris/logic/ui/RootScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import io.github.landrynorris.logic.logic.RootLogic
 import io.github.landrynorris.logic.logic.RootState
 import io.github.landrynorris.logic.logic.SensorState
@@ -19,14 +20,15 @@ import io.github.landrynorris.logic.logic.VoltageComponent
 @Composable
 fun RootScreen(logic: RootLogic) {
     val state by logic.state.collectAsState()
+    val childState = logic.child.subscribeAsState()
 
-    val activeComponent = state.activeComponent
+    val activeChild = childState.value.child
 
-    if(activeComponent == null) {
+    if(activeChild == null) {
         SelectDeviceScreen(state, logic::selectDevice)
     } else {
-        when(activeComponent) {
-            is VoltageComponent -> VoltageScreen(activeComponent)
+        when(val component = activeChild.instance) {
+            is VoltageComponent -> VoltageScreen(component)
         }
     }
 }
