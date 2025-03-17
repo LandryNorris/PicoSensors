1a67d93f623dcc9d60e25cfc77fc7cd79e8c1d71
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-03-16 20:23:28 -0500

    Add spotless, and license headers

--- SensorClient/build.gradle.kts
+++ SensorClient/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
     kotlin("plugin.serialization") version "2.1.10"
+    id("com.diffplug.spotless") version "7.0.2"
 }
 
 group = "io.github.landrynorris"
@@ -37,6 +38,31 @@ dependencies {
     implementation("org.jetbrains.lets-plot:lets-plot-compose:2.1.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+
+        // Add Apache 2.0 license header to all files
+        licenseHeader("""
+            /*
+             * Copyright ${"$"}YEAR Landry Norris
+             * 
+             * Licensed under the Apache License, Version 2.0 (the "License");
+             * you may not use this file except in compliance with the License.
+             * You may obtain a copy of the License at
+             *
+             *     http://www.apache.org/licenses/LICENSE-2.0
+             *
+             * Unless required by applicable law or agreed to in writing, software
+             * distributed under the License is distributed on an "AS IS" BASIS,
+             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+             * See the License for the specific language governing permissions and
+             * limitations under the License.
+             */
+        """.trimIndent())
+    }
 }
 
 compose.desktop {
